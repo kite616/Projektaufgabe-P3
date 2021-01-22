6b4ac572b3c83f254fc0345a93aac299c35d80a5
kite616 <kite.616@gmail.com>
2021-01-22 14:08:45 +0100

    finished

--- dist/js/turingautomata.js
+++ dist/js/turingautomata.js
@@ -61,6 +61,14 @@ class TuringAutomata {
         this.currentstep = 0;
         this.timer = null;
 
+        // SVG-Document
+        this.svg = null;
+
+        window.addEventListener("load", element => {
+            let container = document.getElementById("svg");
+            this.svg = container.contentDocument;
+        });
+
         // HTML-Elements
         this.controls = document.getElementById('acontrols');
         this.table = document.getElementById('sequenztable');
@@ -104,6 +112,7 @@ class TuringAutomata {
         this.resetHighlight();
         document.getElementById('aspeed').value = 5;
         document.getElementById('aspeeddisplay').innerText = '5s';
+        this.table.innerHTML = '';
     }
 
     first() {
@@ -142,35 +151,75 @@ class TuringAutomata {
 
     setHiglights() {
         this.resetHighlight();
-        this.highlightState();
-        this.highlightTransition();
+        setTimeout(() => {  this.highlightState(); }, 300);
+        setTimeout(() => {  this.highlightTransition(); }, 300);        
     }
 
     highlightState() {
+        let state = this.steps[this.currentstep][2];
+        let status = this.steps[this.currentstep][4];
+        let path = this.svg.getElementById('s'+state);
+        let text = this.svg.getElementById('s'+state+'-text');
 
+        if(status !== 'abgelehnt') {
+            if(state === 0 || state === 'a') {
+                this.svg.getElementById('s'+state+'-inner').classList.add('state-selected');
+                this.svg.getElementById('s'+state+'-outer').classList.add('state-selected');
+            } else {
+                path.classList.add('state-selected');
+            }
+            text.classList.add('state-text-selected');
+        } else {
+            if(state === 0 || state === 'a') {
+                this.svg.getElementById('s'+state+'-inner').classList.add('state-failed');
+                this.svg.getElementById('s'+state+'-outer').classList.add('state-failed');
+            } else {
+                path.classList.add('state-failed');
+            }
+            text.classList.add('state-text-failed');
+        }
     }
 
     highlightTransition() {
+        let currentstate = this.steps[this.currentstep][2];
+        let nextstate = this.steps[this.currentstep][4];
 
+        let path = this.svg.getElementById('t-'+currentstate+'-'+nextstate);
+        let text = this.svg.getElementById('t-'+currentstate+'-'+nextstate+'-text');
+
+        console.log(path);
+        console.log(text);
+
+        if(this.steps[this.currentstep][4] !== 'abgelehnt') {
+            try {
+                path.classList.add('transition-selected');
+                text.classList.add('transition-text-selected');
+            } catch (e){}
+        } else {
+            try {
+                path.classList.add('transition-failed');
+                text.classList.add('transition-text-failed');
+            } catch(e){}
+        }
     }
 
     resetHighlight() {
-        document.querySelectorAll(".state").forEach(state => {
+        this.svg.querySelectorAll(".state").forEach(state => {
             state.classList.remove('state-selected');
             state.classList.remove('state-failed');
         });
 
-        document.querySelectorAll(".state-text").forEach(state => {
-            state.classList.remove('state-text-selected');
-            state.classList.remove('state-text-failed');
+        this.svg.querySelectorAll(".state-text").forEach(statetext => {
+            statetext.classList.remove('state-text-selected');
+            statetext.classList.remove('state-text-failed');
         });
 
-        document.querySelectorAll(".transition").forEach(transition => {
+        this.svg.querySelectorAll(".transition").forEach(transition => {
             transition.classList.remove('transition-selected');
             transition.classList.remove('transition-failed');
         });
 
-        document.querySelectorAll(".transition-text").forEach(transitiontext => {
+        this.svg.querySelectorAll(".transition-text").forEach(transitiontext => {
             transitiontext.classList.remove('transition-text-selected');
             transitiontext.classList.remove('transition-text-failed');
         });
@@ -190,8 +239,8 @@ class TuringAutomata {
             let found = false;
             this.turingprogram.forEach(element => {
                 if(state === element[0] && tape[tapeposition] === element[1]) {
-                    // push step: tape, tapeposition, current state, current symbol
-                    this.steps.push([tape.toString(), tapeposition, element[0], element[1], element[1]]);
+                    // push step: tape, tapeposition, current state, current symbol, next state
+                    this.steps.push([tape.toString(), tapeposition, element[0], element[1], element[2]]);
 
                     // update automaton
                     tape[tapeposition] = element[3];
@@ -199,29 +248,36 @@ class TuringAutomata {
                     tapeposition += (element[4] === 'R') ? 1 : -1;
 
                     found = true;
-                    if(element[2] === acceptstate) {
+                    if(element[2] === acceptstate && this.isTapeBlank(tape)) {
                         finished = true;
                         tape[tape.length-1] = 'blank';
-                        this.steps.push([tape.toString(), tapeposition+1, acceptstate, element[3], '']);
+                        this.steps.push([tape.toString(), tapeposition+1, acceptstate, element[3], 'akzeptiert']);
                     }
                 }
             });
             if(!found) {
-                this.steps.push([tape.toString(), tapeposition, 'abgelehnt', tape[tapeposition], '']);
+                this.steps.push([tape.toString(), tapeposition, state, tape[tapeposition], 'abgelehnt']);
                 finished = true;
             }
         }
+    }
+
+    isTapeBlank(tape) {
+        let notblanks = 0;
+        tape.forEach(symbol => {
+            if(symbol !== 'blank') notblanks++;
+        });
+        return (notblanks===0);
     }
 
     // Returns current steps as formatted HTML
     getStepsAsTable() {
         let table = '';
         for(let i=0; i<=this.currentstep; i++) {
-            console.log(i +': ' +this.steps[i]);
             table += '<tr><td>'+this.steps[i][0]+'</td><td>'+this.steps[i][1]+'</td><td>'+this.steps[i][2]+'</td><td>'+this.steps[i][3]+'</td><td>'+this.steps[i][4]+'</td></tr>';
         }
         if(this.currentstep === this.steps.length-1) {
-            if(this.steps[this.steps.length-1][2] === 'a') {
+            if(this.steps[this.steps.length-1][4] !== 'abgelehnt') {
                 table += '<tr><td colspan="5" class="ok">Eingabe ist gültig!</td></tr>';
                 this.info.classList.add('ok');
                 this.info.innerText = 'Eingabe ist gültig!';
